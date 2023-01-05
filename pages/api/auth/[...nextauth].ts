e4f3cfe2d2fc5c16444bf436c191160c0e5ff1e0
Cendy <cendyemail@gmail.com>
2023-01-06 01:01:17 +0700

    fix/black flash

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,3 @@
-import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
@@ -25,15 +24,6 @@ export default NextAuth({
       }
     }
   },
-  adapter: FirestoreAdapter({
-    apiKey: process.env.FIREBASE_API_KEY,
-    appId: process.env.FIREBASE_APP_ID,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  }),
   pages: {
     signIn: "/login",
     newUser: "/join"
