355cd7952b17ddf598b598f4ff9d386141436b75
Cendy <cendyemail@gmail.com>
2023-01-16 00:25:17 +0700

    feat/added small animation to brighten your day

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Button, Card, CardBody, CardHeader, Center, Grid, GridItem, Heading, HStack, Text, Icon } from '@chakra-ui/react'
 import { BsCash, BsFillEmojiHeartEyesFill, BsFillLightningChargeFill } from "react-icons/bs"
 import Head from 'next/head'
+import { useEffect, useRef } from 'react'
+import { useInView, motion, useAnimationControls } from 'framer-motion'
 
 export default function Home() {
+  const ref = useRef(null)
+  const isInView = useInView(ref)
+  const controls = useAnimationControls()
+
+  useEffect(() => {
+    if (isInView) {
+      controls.start("hidden")
+    } else {
+      controls.start("show")
+    }
+  }, [isInView, controls])
+
   return (
     <>
       <Head>
@@ -14,14 +28,14 @@ export default function Home() {
       <Center minH="100vh" flexDir="column">
         <Heading as="h1" size="4xl">Linkku</Heading>
         <Text my="4">Buat halaman terpersonalisasimu dengan mudah</Text>
-        <HStack spacing="2">
+        <HStack spacing="2" ref={ref}>
           <Button as="a" colorScheme="blue" href={process.env.NEXTAUTH_URL! + "/signup"}>Mulai Secara Gratis</Button>
           <Button as="a" variant="outline" href={process.env.NEXTAUTH_URL! + "/signin"}>Masuk</Button>
         </HStack>
       </Center>
       <Center my="32">
-        <Grid templateRows={["repeat(1, 1fr)", "repeat(2, 1fr)"]} templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]} gap={4} maxW="3xl" px={{ base: "4" }} alignItems="center">
-          <GridItem rowSpan={2}>
+        <Grid as={motion.div} variants={staggerContainer} initial="hidden" whileInView="show" viewport={{ once: true }} templateRows={["repeat(1, 1fr)", "repeat(2, 1fr)"]} templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]} gap={4} maxW="3xl" px={{ base: "4" }} alignItems="center">
+          <GridItem as={motion.div} variants={staggerItem} rowSpan={2}>
             <Card>
               <CardHeader>
                 <Icon as={BsCash} w={50} h={50} my="2"/>
@@ -32,7 +46,7 @@ export default function Home() {
               </CardBody>
             </Card>
           </GridItem>
-          <GridItem>
+          <GridItem as={motion.div} variants={staggerItem}>
             <Card>
               <CardHeader>
                 <Icon as={BsFillEmojiHeartEyesFill} w={50} h={50} my="2"/>
@@ -43,7 +57,7 @@ export default function Home() {
               </CardBody>
             </Card>
           </GridItem>
-          <GridItem>
+          <GridItem as={motion.div} variants={staggerItem}>
             <Card>
               <CardHeader>
                 <Icon as={BsFillLightningChargeFill} w={50} h={50} my="2"/>
@@ -96,6 +110,37 @@ export default function Home() {
       <Center my="10">
         <Text fontWeight="bold">Dibuat dengan 💖 oleh Cendy</Text>
       </Center>
+      <HStack as={motion.div} variants={navVariant} initial="hidden" animate={controls} justifyContent="space-between" px="6" pos="fixed" inset="0" h="max-content" borderBottom="1px" borderColor="gray.200" bg="white">
+        <Text fontWeight="bold" fontSize="3xl">Linkku</Text>
+        <HStack spacing="2">
+          <Button as="a" size={["xs", "sm"]} colorScheme="blue" href={process.env.NEXTAUTH_URL! + "/signup"}>Mulai Secara Gratis</Button>
+          <Button as="a" size={["xs", "sm"]} variant="outline" href={process.env.NEXTAUTH_URL! + "/signin"}>Masuk</Button>
+        </HStack>
+      </HStack>
     </>
   )
+}
+
+const navVariant = {
+  hidden : {
+    translateY : "-100%"
+  },
+  show : {
+    translateY : 0
+  }
+}
+
+const staggerContainer = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5
+    }
+  }
+}
+
+const staggerItem = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 }
 }
